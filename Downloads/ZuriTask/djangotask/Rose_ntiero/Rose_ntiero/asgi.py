56573213c0e9a0594f17907a79672bc8c55227da
Rosella <ntierorossy@gmail.com>
2022-06-05 20:30:16 +0100

    django project

---
+++ Downloads/ZuriTask/djangotask/Rose_ntiero/Rose_ntiero/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Rose_ntiero project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rose_ntiero.settings')
+
+application = get_asgi_application()
---
+++ Downloads/ZuriTask/djangotask/Rose_ntiero/Rose_ntiero/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Rose_ntiero project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rose_ntiero.settings')
+
+application = get_wsgi_application()
